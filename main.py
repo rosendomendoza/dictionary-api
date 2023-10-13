1c31f65866f1b1d0c59ebf31e5e3e815259bac79
ROSENDO MENDOZA <rosendo.arturo@gmail.com>
2023-10-13 14:44:42 +0200

    Make the Website and the api skeleton

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
+app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return render_template("dictionary.html")
+
+@app.route("/api/v1/<word>")
+def api(word):
+    definition = "The word´s definition"
+    return (definition)
+
+app.run(debug=True)
